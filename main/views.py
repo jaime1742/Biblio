9366a7b172f4f1d39da8d79650883f09f7dbad54
Rodrigo <rrddpp1@gmail.com>
2023-11-11 12:14:53 +0100

    Implementación del login

--- main/views.py
+++ main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
 
 
+@login_required
 def index(request):
     return render(request, 'index.html', {
         'users': 'HOLA',
