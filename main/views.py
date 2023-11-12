1dabc14258b3a8cd8fec8ec13ee7553b38d9768f
Rodrigo <rrddpp1@gmail.com>
2023-11-12 18:12:42 +0100

    Quitar import innecesario

--- main/views.py
+++ main/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
 from .models import User
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.db import IntegrityError
 from .forms import UserRegistrationForm
