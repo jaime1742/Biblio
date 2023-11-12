b9bc21787beb593feaf1e277d3056a60bcefb92a
Rodrigo <rrddpp1@gmail.com>
2023-11-12 12:33:38 +0100

    Cambiar la implementación de registro creada con UserCreationForm a una totalmente personalizada para mayor comodidad con los textos.

--- main/views.py
+++ main/views.py
@@ -5,6 +5,7 @@ from .models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.db import IntegrityError
+from .forms import UserRegistrationForm
 
 
 @login_required
@@ -15,18 +16,27 @@ def index(request):
 
 
 def signup(request):
-    if request.method == 'GET':
-        return render(request, 'registration/signup.html', {"form": UserCreationForm})
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            password_confirm = form.cleaned_data['password_confirm']
+
+            if password == password_confirm:
+                try:
+                    user = User.objects.create_user(username=username, password=password)
+                    user.save()
+                    login(request, user)
+                    return redirect('/')
+                
+                except IntegrityError:
+                    return render(request, 'registration/signup.html', {'form': form, 'error': 'Este usuario ya existe.'})
+            
+            return render(request, 'registration/signup.html', {'form': form, 'error': 'Las contraseñas no coinciden'})
+    
     else:
+        form = UserRegistrationForm()
 
-        if request.POST["password1"] == request.POST["password2"]:
-            try:
-                user = User.objects.create_user(
-                    request.POST["username"], password=request.POST["password1"])
-                user.save()
-                login(request, user)
-                return redirect('/')
-            except IntegrityError:
-                return render(request, 'registration/signup.html', {"form": UserCreationForm, "error": "Este usuario ya existe."})
-
-        return render(request, 'registration/signup.html', {"form": UserCreationForm, "error": "Las contraseñas no coinciden."})
+    return render(request, 'registration/signup.html', {'form': form})
