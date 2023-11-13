be777dc398c6fbad7ec89d80146dfd9134515d2b
Edu <eduardo.salazar.ceb@immune.institute>
2023-11-13 12:19:02 +0100

    edu

--- main/views.py
+++ main/views.py
@@ -1,3 +1,14 @@
+<<<<<<< Updated upstream
 from django.shortcuts import render
+=======
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
+from .models import User, Vehiculo
+from django.contrib.auth import login
+from django.db import IntegrityError
+from .forms import UserRegistrationForm
+from .models import Vehiculo
+>>>>>>> Stashed changes
 
 # Create your views here.
