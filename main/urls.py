be4674d2fc70143c5c667d9cb73da3f0e4a26f07
Rodrigo <rrddpp1@gmail.com>
2023-11-21 19:52:28 +0100

    dsad

--- main/urls.py
+++ main/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('coches/filtro/', views.filtro_coches, name='filtro_coches'),
     path('api/users/', views.users_list, name='users-list'),
     path('api/user/<int:pk>/', views.user_profile, name='user-profile'),
-    path('add_email/', views.update_email, name='add-email'),
+    path('add_email/', views.add_email, name='add-email'),
     path('update-username/', views.update_username, name='update-username'),
     path('coches/<int:pk>', views.vehiculo_detalle, name='vehiculo_detalle'),
 ]
